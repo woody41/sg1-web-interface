2ac8d2812493a37102fdb9099d008c781bd55201
Matej Seifert <matej.seifert@lundegaard.eu>
2023-08-01 06:13:08 +0200

    stargate done

--- js/generateLines.js
+++ js/generateLines.js
@@ -1,4 +1,4 @@
-function drawLineBetweenDivs(div1Id, div2Id, anchor1, anchor2) {
+function drawLineBetweenDivs(div1Id, div2Id, anchor1, anchor2, extraClass) {
     const div1 = document.getElementById(div1Id);
     const div2 = document.getElementById(div2Id);
 
@@ -43,6 +43,7 @@ function drawLineBetweenDivs(div1Id, div2Id, anchor1, anchor2) {
     // Create a new line div
     const line = document.createElement('div');
     line.classList.add('generated-line');
+    line.classList.add(extraClass);
 
     // Position and rotate the line
     line.style.top = anchorPoint1.y + 'px';
@@ -57,7 +58,14 @@ function drawLineBetweenDivs(div1Id, div2Id, anchor1, anchor2) {
 
 
 // Call the function with the IDs of the divs and anchor sides
-drawLineBetweenDivs('glyph7-a', 'glyph7-b', 'top', 'right');
-drawLineBetweenDivs('glyph6-a', 'glyph6-b', 'top', 'right');
-drawLineBetweenDivs('glyph5-a', 'glyph5-b', 'top', 'right');
-drawLineBetweenDivs('glyph4-a', 'glyph4-b', 'top', 'right');
+drawLineBetweenDivs('glyph7-a', 'glyph7-b', 'top', 'right', 'line-chevron7');
+drawLineBetweenDivs('glyph6-a', 'glyph6-b', 'top', 'right', 'line-chevron6');
+drawLineBetweenDivs('glyph5-a', 'glyph5-b', 'top', 'right', 'line-chevron5');
+drawLineBetweenDivs('glyph4-a', 'glyph4-b', 'top', 'right', 'line-chevron4');
+drawLineBetweenDivs('glyph1-a', 'glyph1-b', 'left', 'right', 'line-chevron1');
+drawLineBetweenDivs('glyph2-a', 'glyph2-b', 'left', 'right', 'line-chevron2');
+drawLineBetweenDivs('glyph3-a', 'glyph3-b', 'left', 'right', 'line-chevron3');
+drawLineBetweenDivs('glyph4-b', 'glyph4-c', 'left', 'right', 'line-chevron4');
+drawLineBetweenDivs('glyph7-b', 'glyph7-c', 'left', 'right', 'line-chevron7');
+drawLineBetweenDivs('glyph6-b', 'glyph6-c', 'left', 'right', 'line-chevron6');
+drawLineBetweenDivs('glyph5-b', 'glyph5-c', 'left', 'right', 'line-chevron5');
